d918f2a5905a4a7990f6a19bf50ae061e4088aca
Natt <natt.lefevre@gmail.com>
2026-04-17 07:47:16 -0700

    chore(ui): bump version of @nattstack/ui to 0.0.42 and add isMasked prop to OTPField component

--- packages/ui/src/components/otp-field/otp-field.tsx
+++ packages/ui/src/components/otp-field/otp-field.tsx
@@ -5,17 +5,25 @@ import styles from "./otp-field.module.css"
 
 interface OTPFieldProps extends Omit<OTPFieldBase.Root.Props, "disabled"> {
   isDisabled?: OTPFieldBase.Root.Props["disabled"]
+  isMasked?: OTPFieldBase.Root.Props["mask"]
 }
 
 export function OTPField(props: OTPFieldProps): JSX.Element {
-  const { className: customClassName = "", isDisabled = false, ...rest } = props
+  const { className: customClassName = "", isDisabled = false, isMasked = false, ...rest } = props
 
   const combinedClassName = normalizeWhitespace(`
     ${OTP_FIELD_CLASS_NAME.BASE}
     ${customClassName}
   `)
 
-  return <OTPFieldBase.Root className={combinedClassName} disabled={isDisabled} {...rest} />
+  return (
+    <OTPFieldBase.Root
+      className={combinedClassName}
+      disabled={isDisabled}
+      mask={isMasked}
+      {...rest}
+    />
+  )
 }
 
 export const OTP_FIELD_CLASS_NAME = {
