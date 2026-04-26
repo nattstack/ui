b21fae66de4d7a145ad27a4e1bdf975b80f48c89
Natt <natt.lefevre@gmail.com>
2026-04-25 20:39:07 -0700

    refactor(menu): remove MenuItemContent component and simplify MenuItem, MenuLinkItem, and MenuSubmenuTrigger implementations

--- packages/ui/src/components/menu/menu-item.tsx
+++ packages/ui/src/components/menu/menu-item.tsx
@@ -1,25 +1,15 @@
 import { Menu as BaseMenu } from "@base-ui/react"
-import type { JSX, ReactNode } from "react"
+import type { JSX } from "react"
 import { MENU_CLASS_NAME } from "./menu-class-names"
-import { MenuItemContent } from "./menu-item-content"
 import { getMenuClassName } from "./menu-utils"
 
 export interface MenuItemProps extends Omit<BaseMenu.Item.Props, "className" | "disabled"> {
   className?: string
-  iconEnd?: ReactNode
-  iconStart?: ReactNode
   isDisabled?: BaseMenu.Item.Props["disabled"]
 }
 
 export function MenuItem(props: MenuItemProps): JSX.Element {
-  const {
-    children,
-    className: customClassName = "",
-    iconEnd = undefined,
-    iconStart = undefined,
-    isDisabled = false,
-    ...rest
-  } = props
+  const { children, className: customClassName = "", isDisabled = false, ...rest } = props
 
   const combinedClassName = getMenuClassName(MENU_CLASS_NAME.ITEM, customClassName)
 
@@ -30,9 +20,7 @@ export function MenuItem(props: MenuItemProps): JSX.Element {
       disabled={isDisabled}
       {...rest}
     >
-      <MenuItemContent iconEnd={iconEnd} iconStart={iconStart}>
-        {children}
-      </MenuItemContent>
+      {children}
     </BaseMenu.Item>
   )
 }
--- packages/ui/src/components/menu/menu-link-item.tsx
+++ packages/ui/src/components/menu/menu-link-item.tsx
@@ -1,31 +1,14 @@
 import { Menu as BaseMenu } from "@base-ui/react"
-import type { JSX, ReactNode } from "react"
-import { MENU_CLASS_NAME } from "./menu-class-names"
-import { MenuItemContent } from "./menu-item-content"
-import { getMenuClassName } from "./menu-utils"
+import type { JSX } from "react"
 
-export interface MenuLinkItemProps extends Omit<BaseMenu.LinkItem.Props, "className"> {
-  className?: string
-  iconEnd?: ReactNode
-  iconStart?: ReactNode
-}
+export interface MenuLinkItemProps extends BaseMenu.LinkItem.Props {}
 
 export function MenuLinkItem(props: MenuLinkItemProps): JSX.Element {
-  const {
-    children,
-    className: customClassName = "",
-    iconEnd = undefined,
-    iconStart = undefined,
-    ...rest
-  } = props
-
-  const combinedClassName = getMenuClassName(MENU_CLASS_NAME.LINK_ITEM, customClassName)
+  const { children, ...rest } = props
 
   return (
-    <BaseMenu.LinkItem className={combinedClassName} data-slot="menu-link-item" {...rest}>
-      <MenuItemContent iconEnd={iconEnd} iconStart={iconStart}>
-        {children}
-      </MenuItemContent>
+    <BaseMenu.LinkItem data-slot="menu-link-item" {...rest}>
+      {children}
     </BaseMenu.LinkItem>
   )
 }
